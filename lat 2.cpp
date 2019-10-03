871dbaa0f63d906687bb0623d4bdb6d678bab5a5
ayuayaaa <55979247+ayuayaaa@users.noreply.github.com>
2019-10-03 14:33:05 +0700

    Add files via upload

---
+++ lat 2.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int a, t, hasil;
+
+int main(){
+
+printf ("Masukan Alas");scanf ("%d", &a);
+printf ("Masukan Tinggi"); scanf ("%d", &t);
+hasil = (a*t)/2;
+printf ("Luas Segitiga Adalah %d", hasil);
+
+return 0;
+}
---
+++ latihan1.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int x, y, hasil;
+
+int main(){
+
+printf("Masukan Panjang ");scanf("%d", &x);
+printf("Masukan Lebar ");scanf("%d", &y);
+
+hasil = x*y;
+
+printf("Hasilnya adalah %d", hasil);
+
+}
