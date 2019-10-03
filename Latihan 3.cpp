2432fc9b9e7b65d89aa5d74ba536f486f79a31b5
ayuayaaa <55979247+ayuayaaa@users.noreply.github.com>
2019-10-03 14:36:59 +0700

    Add files via upload

---
+++ Latihan 3.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int r, hasil;
+
+int main(){
+
+printf ("Masukkan panjang jari-jari lingkaran"), scanf ("%d", &r);
+
+hasil = ((r*r)*22)/7;
+printf ("Luas lingkaran adalah %d", hasil );
+
+return 0;
+}
